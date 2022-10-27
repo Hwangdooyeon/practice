120596085025dd55b70e2e39d83b93e331880cd4
Hwang doo yeon <109845059+Hwangdooyeon@users.noreply.github.com>
2022-10-27 23:04:48 +0900

    -Homework

---
+++ src/Solution1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        System.out.println("숫자를 입력해주세요");
+
+        for (int i = 1; i < 11; i++){
+           System.out.println(i);
+        }
+
+
+
+
+    }
+}
