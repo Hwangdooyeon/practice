479d2b9eea70b3b2d525b320dab660973f62d3b2
Hwang doo yeon <109845059+Hwangdooyeon@users.noreply.github.com>
2023-04-18 04:08:10 +0900

    -Homework

---
+++ src/Baekjoon11718.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Baekjoon11718 {
+    final static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        while (sc.hasNextLine()) {
+            String S = sc.nextLine();
+            System.out.println(S);
+        }
+    }
+}
