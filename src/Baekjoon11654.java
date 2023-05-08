c5813972a32a724a4172cb9564766f7c2c48530c
Hwang doo yeon <109845059+Hwangdooyeon@users.noreply.github.com>
2023-05-09 00:14:10 +0900

    pratice

---
+++ src/Baekjoon11654.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Baekjoon11654 {
+    final static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
+
+        String s = sc.next();
+
+        int result = s.charAt(0);
+
+        System.out.println(result);
+    }
+}
