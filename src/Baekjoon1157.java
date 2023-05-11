a83a1e96453643a6fce18a7f461ef92a97b72b6d
Hwang doo yeon <109845059+Hwangdooyeon@users.noreply.github.com>
2023-05-12 01:50:20 +0900

    pratice

--- src/Baekjoon1157.java
+++ src/Baekjoon1157.java
@@ -1,8 +1,34 @@
 import java.util.Scanner;
 
 public class Baekjoon1157 {
+    final static Scanner sc = new Scanner(System.in);
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine().toUpperCase();
+        int[] count = new int[26];
+
+        int max = 0;
+        char maxchar = '?';
+
+        for (int i=0; i<s.length(); i++){
+            int idx = s.charAt(i) - 'A';
+            ++count[idx];
+
+            if (max < count[idx]){
+                max = count[idx];
+                maxchar = s.charAt(i);
+
+
+            }
+            else if (max == count[idx]){
+                maxchar = '?';
+            }
+        }
+
+        System.out.println(maxchar);
+
+
+
 
     }
 }
