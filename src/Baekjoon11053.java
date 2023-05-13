165a9620ce20be269f5e0a302e666023351d96c6
Hwang doo yeon <109845059+Hwangdooyeon@users.noreply.github.com>
2023-05-14 00:08:51 +0900

    pratice

---
+++ src/Baekjoon11053.java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Baekjoon11053 {
+    final static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
+
+        int n = sc.nextInt();
+        int [] arr = new int[n];
+        int [] dt = new int[n];
+
+        for (int i=0; i<n; i++){
+
+            arr[i] = sc.nextInt();
+            dt[i] = 1;
+        }
+        for (int i=0; i<n; i++){
+            for(int j= i; j<n; j++){
+                  if (arr[i] < arr[j]){
+                      dt[j] = Math.max(dt[j], dt[i] +1);
+                  }
+            }
+        }
+        int max = 0;
+        for (int i=0; i<n; i++){
+            if (max < dt[i])
+                max = dt[i];
+
+            }
+            System.out.println(max);
+        }
+    }
+
