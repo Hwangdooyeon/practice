3827aaf9c063fa12de019963a2dc183b6803da51
Hwang doo yeon <109845059+Hwangdooyeon@users.noreply.github.com>
2023-05-26 01:35:00 +0900

    pratice

--- src/Baekjoon2559.java
+++ src/Baekjoon2559.java
@@ -2,29 +2,32 @@ import java.util.Scanner;
 
 public class Baekjoon2559 {
     final static Scanner sc = new Scanner(System.in);
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         int n = sc.nextInt();
         int k = sc.nextInt();
+        int maxV = -10000000;
+        int[] A = new int[n];
 
-        int [] arr = new int[n];
+        for (int i = 0; i < n; i++) {
 
-        int max = Integer.MIN_VALUE;
-        for (int i=0; i<n; i++){
-
-                 arr[i] = sc.nextInt();
-            }
-        for (int i=0; i<=n-k; i++){
-            int sum = 0;
-           for (int j=i; j<i+k; j++){
-
-               sum += arr[j];
-           }
-           max = Math.max(max, sum);
-
-
+            A[i] = sc.nextInt();
         }
-        System.out.println(max);
+
+        for (int u=0; u<=n-k; u++){
+            int sum = 0;
+            for (int x=u; x<u+k; x++){
+
+                sum += A[x];
+
+
+            }
+            if (maxV < sum){
+
+                maxV = sum;
+            }
+        }
+        System.out.println(maxV);
     }
 }
 
