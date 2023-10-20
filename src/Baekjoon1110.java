3fa42d77faeea0898138173cece57d7ebbb4d95e
Hwangdooyeon <yueuch102@naver.com>
2023-10-20 11:40:18 +0900

    pratice

--- src/Baekjoon1110.java
+++ src/Baekjoon1110.java
@@ -1,20 +1,23 @@
 import java.util.Scanner;
 
 public class Baekjoon1110 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int sum = 0;
-        int N = sc.nextInt();
-        int arr[] = new int[N];
 
-        for (int i=0; i<N; i++){
-            int sum1 = 0;
-            int sum2 = 0;
-            int result = 0;
+    final static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
 
-          sum = sum1 + sum2;
+        int n = sc.nextInt();
 
-          result = sum;
+        int cnt = 0;
+        int answer = n;
+
+        while (true){
+
+            n = ((n % 10) * 10) + (((n / 10) + (n % 10)) % 10);
+            cnt++;
+            if (answer==n){
+                break;
+            }
         }
+        System.out.println(cnt);
     }
 }
